2b7e00585ae852db4283e78b026803b3498807a7
cph-sn311 <cph-sn311@cphbusiness.dk>
2022-05-11 22:27:27 +0200

    removed sidebar

--- Website/Data/DataFetcher.cs
+++ Website/Data/DataFetcher.cs
@@ -11,12 +11,12 @@ public class DataFetcher
         _httpClient = httpClient;
     }
 
-    public Task<Root<AverageWageDatum>> GetAverageWages()
+    public Task<Root<AverageWageDatum>> GetAverageWages(bool useCache)
     {
         return _httpClient.GetFromJsonAsync<Root<AverageWageDatum>>("https://datausa.io/api/data?measures=Average%20Wage,Average%20Wage%20Appx%20MOE&drilldowns=Detailed%20Occupation");
     }
 
-    public Task<Root<TotalNoninstructionalEmployeesDatum>> GetTotalNoninstructionalEmployees()
+    public Task<Root<TotalNoninstructionalEmployeesDatum>> GetTotalNoninstructionalEmployees(bool useCache)
     {
         return _httpClient.GetFromJsonAsync<Root<TotalNoninstructionalEmployeesDatum>>("https://datausa.io/api/data?University=142832&measures=Total%20Noninstructional%20Employees&drilldowns=IPEDS%20Occupation&parents=true");
     }
